7e631c197f22a2b40930153af812debb2203faf9
ayobami1134 <ayobami1134@gmail.com>
2025-02-19 19:37:14 +0100

    added newline character and comment to C file

--- C/intro.c
+++ C/intro.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
+#include <stdio.h>  // Include the Standard Input Output library for input and output functions
 
-int main() {
-    printf("Hello world");
-    return 0;
+/* 
+multi-line comment
+*/
+
+int main() {  // Define the main function, the entry point of the program
+    printf("Hello world\n");  // Print the string "Hello world" followed by a newline character to the console
+    return 0;  // Return 0 to indicate that the program executed successfully
 }
+
